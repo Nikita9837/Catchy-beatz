601b53e706c48a85bb7b3f6418790ded80f15d67
PSupriya12 <90033745+PSupriya12@users.noreply.github.com>
2021-12-20 20:51:30 +0530

    Add files via upload

---
+++ MusicList.js
@@ -0,0 +1,129 @@
+let allMusic = [
+    {
+        name : "Kal ho na ho", 
+        artist : "Sonu Nigam",
+        img : "",
+        src : "",
+    },
+    {
+        name : "Chill vibes",
+        artist : "james",
+        img : "",
+        src : "",
+    },
+    {
+        name : "Crazy",
+        artist : "Andrew",
+        img : "",
+        src : "",
+    },
+    {
+        name : "Dil Dhadakne Do",
+        artist : "Joi Barua,Shankar Mahadevan",
+        img : "",
+        src : "",
+    },
+    {
+        name : "Love me like you Do ",
+        artist : "Ellie Goulding ",
+        img : "",
+        src : "",
+    },
+    {
+        name : "Lonely",
+        artist : "Christopher",
+        img : "",
+        src : "",
+    },
+    {
+        name : "Maine tere liye",
+        artist : "Mukesh",
+        img : "",
+        src : "",
+    },
+    {
+        name : "Cheap thrills",
+        artist : "Sia",
+        img : "",
+        src : "",
+    },
+    {
+        name : "Memories",
+        artist : "Maroon 5",
+        img : "",
+        src : "",
+    },
+    {
+        name : "Gagaku",
+        artist : "Kamisato Ayaka",
+        img : "",
+        src : "",
+    },
+    {
+        name : "Aashiyan",
+        artist : "Shreya Ghoshal,Nikhil Paul George",
+        img : "",
+        src : "",
+    },
+    {
+        name : "Kuch to bata zindagi",
+        artist : "Jubin Nautiyal",
+        img : "",
+        src : "",
+    },
+    {
+        name : "Life goes on",
+        artist : "BTS",
+        img : "",
+        src : "",
+    },
+    {
+        name : "Celebrity",
+        artist : "IU",
+        img : "",
+        src : "",
+    },
+    {
+        name : "Cosmic chill",
+        artist : "Lofi girl",
+        img : "",
+        src : "",
+    },
+    {
+        name : "Believer",
+        artist : "Imagine Dragons",
+        img : "",
+        src : "",
+    },
+    {
+        name : "The Nights",
+        artist : "Avicii",
+        img : "",
+        src : "",
+    },
+    {
+        name : "On & on",
+        artist : "Cartoon",
+        img : "",
+        src : "",
+    },
+    {
+        name : "Dhagaa",
+        artist : "Nilotpal Bora",
+        img : "",
+        src : "",
+    },
+    {
+        name : "Alone",
+        artist : "Marshmello",
+        img : "",
+        src : "",
+    },
+    {
+        name : "Euphoria",
+        artist : "BTS",
+        img : "",
+        src : "",
+    }
+
+]
